94e169851bbd2ec510bb3fe0750dd489784808f1
Petri Kallio <kallionpetri@gmail.com>
2015-01-28 20:59:58 +0200

    Keskiviikon välipush

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,23 @@
 Rails.application.routes.draw do
+  resources :users
+
   resources :beers
 
   resources :breweries
+
+  resources :ratings, only: [:index, :new, :create, :destroy]
+
+  resource :session, only: [:new, :create, :delete]
 
   root 'breweries#index'
 
   get 'kaikki_bisset', to: 'beers#index'
 
+  get 'signup', to:'users#new'
+
 #  get 'ratings', to: 'ratings#index'
 #  get 'ratings/new', to: 'ratings#new'
 #  post 'ratings', to:'ratings#create'
-
-  resources :ratings, only: [:index, :new, :create, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
